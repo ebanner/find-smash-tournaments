db533c7c01a3e27c91db59fd302b533bb4c148ed
Edward Banner <edward.banner@gmail.com>
2025-02-03 11:05:21 -0500

    Add future tournaments
    start.gg doesn't list future Events as having attendees, but they do
    list future Tournaments as having them.
    
    So we make an extra query to see all the tournaments a player is in,
    filter them down to future tournaments, and add them to the overall
    list.

--- etl/lambda_function.py
+++ etl/lambda_function.py
@@ -3,6 +3,8 @@ import pandas as pd
 from datetime import datetime
 import requests
 
+import time
+
 import os
 
 import boto3
@@ -10,6 +12,9 @@ from botocore.exceptions import ClientError
 
 import gspread
 from gspread_dataframe import set_with_dataframe
+
+
+START_GG_API_URL = 'https://api.start.gg/gql/alpha'
 
 
 def get_credentials_dict():
@@ -46,66 +51,125 @@ gsheets_client = gspread.service_account_from_dict(credentials_dict)
 spreadsheet = gsheets_client.open('Smash Tournaments')
 
 
-def fetch_tournament_data(player_id, page, PER_PAGE=490):
-    url = 'https://api.start.gg/gql/alpha'
-    
-    query = """
-    query PlayerQuery {
-      player(id: "{player_id}") {
-        user {
-          events(query: { page: {page}, perPage: {per_page} }) {
-            nodes {
-              tournament {
-                name
+def get_event_df(player):
+    def fetch_tournament_data(player_id, page, PER_PAGE=490):
+        
+        query = """
+        query PlayerQuery {
+          player(id: "{player_id}") {
+            user {
+              events(query: { page: {page}, perPage: {per_page} }) {
+                nodes {
+                  tournament {
+                    name
+                  }
+                  slug
+                  startAt
+                }
               }
-              slug
-              startAt
             }
           }
+        }""".replace('{page}', str(page)).replace('{player_id}', str(player_id)).replace('{per_page}', str(PER_PAGE))
+        
+        token = os.environ['START_GG_TOKEN']
+        headers = { 
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'  # if authentication is needed
+        }   
+        
+        response = requests.post(START_GG_API_URL, json={'query': query}, headers=headers)
+        result = response.json()
+    
+        return result
+    
+    def get_tournaments(result):
+        tournaments = result['data']['player']['user']['events']['nodes']
+        for tournament in tournaments:
+            tournament['name'] = tournament['tournament']['name']
+            del tournament['tournament']
+        return tournaments
+    
+    def make_df(gamertag, tournaments):
+        df = pd.DataFrame(tournaments)
+        df = df.drop_duplicates().reset_index(drop=True)
+        df['gamertag'] = gamertag
+        reordered_df = df[['gamertag', 'name', 'slug', 'startAt']]
+        return reordered_df
+    
+    def get_all_tournaments(player_id):
+        page = 1
+        all_tournaments = []
+        while True:
+            # print('page', page)
+            result = fetch_tournament_data(player_id, page)
+            tournaments = get_tournaments(result)
+            if tournaments == []:
+                break
+            all_tournaments.extend(tournaments)
+            page += 1
+        return all_tournaments
+
+    tournaments = get_all_tournaments(player['id'])
+    event_df = make_df(player['gamertag'], tournaments)
+    event_df['type'] = 'event'
+    return event_df
+
+
+def get_future_tournament_df(player):
+    def fetch_tournaments(player_id, page):
+        query = """
+        query UserTournaments($playerId: ID!, $perPage: Int, $page: Int) {
+          player(id: $playerId) {
+            user {
+              tournaments(query: { perPage: $perPage, page: $page, sortBy: "startAt" }) {
+                nodes {
+                  name
+                  startAt
+                  slug
+                }
+              }
+            }
+          }
+        }"""
+
+        variables = {
+            "playerId": player_id,
+            "perPage": 390,
+            "page": page
+
         }
-      }
-    }""".replace('{page}', str(page)).replace('{player_id}', str(player_id)).replace('{per_page}', str(PER_PAGE))
-    
-    token = os.environ['START_GG_TOKEN']
-    headers = { 
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {token}'  # if authentication is needed
-    }   
-    
-    response = requests.post(url, json={'query': query}, headers=headers)
-    result = response.json()
 
-    return result
+        token = os.environ['START_GG_TOKEN']
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'  # if authentication is needed
+        }
 
+        response = requests.post(START_GG_API_URL, json={'query': query, 'variables': variables}, headers=headers)
+        result = response.json()
 
-def get_tournaments(result):
-    tournaments = result['data']['player']['user']['events']['nodes']
-    for tournament in tournaments:
-        tournament['name'] = tournament['tournament']['name']
-        del tournament['tournament']
-    return tournaments
+        tournaments = result['data']['player']['user']['tournaments']['nodes']
 
+        return tournaments
 
-def make_df(gamertag, tournaments):
-    df = pd.DataFrame(tournaments)
-    df = df.drop_duplicates().reset_index(drop=True)
-    df['gamertag'] = gamertag
-    reordered_df = df[['gamertag', 'name', 'slug', 'startAt']]
-    return reordered_df
+    def get_tournaments(player_id):
+        all_tournaments = []
+        page = 1
+        while True:
+            print('page', page)
+            tournaments = fetch_tournaments(player_id, page)
+            if tournaments == []:
+                break
+            all_tournaments.extend(tournaments)
+            page += 1
+        return all_tournaments
 
-
-def get_all_tournaments(player_id):
-    page = 1
-    all_tournaments = []
-    while True:
-        # print('page', page)
-        result = fetch_tournament_data(player_id, page)
-        tournaments = get_tournaments(result)
-        if tournaments == []:
-            break
-        all_tournaments.extend(tournaments)
-        page += 1
-    return all_tournaments
+    tournaments = get_tournaments(player['id'])
+    tournament_df = pd.DataFrame(tournaments)
+    future_tournament_df = tournament_df[tournament_df.startAt > time.time()]
+    future_tournament_df['type'] = 'tournament'
+    future_tournament_df['gamertag'] = player['gamertag']
+    return future_tournament_df
 
 
 def lambda_handler(event, context):
@@ -122,14 +186,14 @@ def lambda_handler(event, context):
         {"gamertag": "Kola", "id": 18802},
     ]
     
-    event_df = pd.DataFrame(columns=['gamertag', 'name', 'slug', 'startAt'])
+    df = pd.DataFrame(columns=['gamertag', 'name', 'slug', 'startAt', 'type'])
     for player in players:
-        tournaments = get_all_tournaments(player['id'])
-        df = make_df(player['gamertag'], tournaments)
-        event_df = pd.concat([event_df, df]).reset_index(drop=True)
+        event_df = get_event_df(player)
+        future_tournament_df = get_future_tournament_df(player)
+        df = pd.concat([df, event_df, future_tournament_df]).reset_index(drop=True)
 
     sorted_grouped_data = sorted(
-        event_df.groupby('slug'),
+        df.groupby('slug'),
         key=lambda x: x[1]['startAt'].iloc[0]
     )
     
@@ -139,6 +203,7 @@ def lambda_handler(event, context):
             'slug': group['slug'].iloc[0],
             'startAt': group['startAt'].iloc[0],
             'gamertag': group['gamertag'].tolist(),
+            'type': group['type'].iloc[0]
         }
         for _, group in sorted_grouped_data
     ]
@@ -158,7 +223,7 @@ def lambda_handler(event, context):
                 max_idx = i
         return tournaments[max_idx:max_idx+1]
 
-    tournament_names = event_df.name.unique()
+    tournament_names = df.name.unique()
     deduped_tournaments = []
     for name in tournament_names:
         tournaments_by_name = get_tournaments_by_name(tournaments, name)
@@ -172,7 +237,14 @@ def lambda_handler(event, context):
     #
     tournaments_df = pd.DataFrame(deduped_tournaments)
     tournaments_df['#'] = tournaments_df['gamertag'].map(len)
-    tournaments_df['url'] = tournaments_df['slug'].map(lambda slug: f'https://www.start.gg/{slug}/overview')
+    def get_url(row):
+        print(row)
+        if row['type'] == 'event':
+            url = f'https://www.start.gg/{row.slug}/overview'
+        elif row['type'] == 'tournament':
+            url = f'https://www.start.gg/{row.slug}/details'
+        return url
+    tournaments_df['url'] = tournaments_df.apply(get_url, axis=1)
     tournaments_df['Players'] = tournaments_df['gamertag'].map(lambda gamertag: ', '.join(gamertag))
     tournaments_df['Date'] = tournaments_df['startAt']
 
@@ -189,3 +261,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': f'Success - {num_rows} rows written to Google Sheets!'
     }
+
